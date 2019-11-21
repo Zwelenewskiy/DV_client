df2d60377327c28d759ceaae772c35ceb9fab7be
Александр Пертенава <zwelenewskiy@yandex.ru>
2019-11-21 10:48:00 +0300

    ...

--- DV_client/Connected Services/Server/Reference.cs
+++ DV_client/Connected Services/Server/Reference.cs
@@ -15,7 +15,7 @@ namespace DV_client.Server {
     
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
-    [System.Runtime.Serialization.DataContractAttribute(Name="Email", Namespace="http://schemas.datacontract.org/2004/07/DV_server")]
+    [System.Runtime.Serialization.DataContractAttribute(Name="Email", Namespace="http://schemas.datacontract.org/2004/07/Models")]
     [System.SerializableAttribute()]
     public partial class Email : object, System.Runtime.Serialization.IExtensibleDataObject, System.ComponentModel.INotifyPropertyChanged {
         
