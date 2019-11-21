6ed9b55a0181fed7bf8d99ca1d36d4fcad28c266
Александр Пертенава <zwelenewskiy@yandex.ru>
2019-11-21 19:55:53 +0300

    Добавлено полуение писем с отправителями, копиями и скрытыми копиями

--- DV_client/Form1.Designer.cs
+++ DV_client/Form1.Designer.cs
@@ -28,11 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.DGV_emails = new System.Windows.Forms.DataGridView();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.письмоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.TSMI_saveEmail = new System.Windows.Forms.ToolStripMenuItem();
             this.TSMI_getEmails = new System.Windows.Forms.ToolStripMenuItem();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_emails)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -41,8 +53,19 @@
             // 
             this.DGV_emails.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.DGV_emails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_emails.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column4,
+            this.Column5,
+            this.Column3,
+            this.Column6,
+            this.Column7,
+            this.Column8});
             this.DGV_emails.Location = new System.Drawing.Point(12, 27);
             this.DGV_emails.Name = "DGV_emails";
+            this.DGV_emails.ReadOnly = true;
+            this.DGV_emails.RowHeadersVisible = false;
             this.DGV_emails.Size = new System.Drawing.Size(946, 435);
             this.DGV_emails.TabIndex = 0;
             // 
@@ -68,16 +91,72 @@
             // TSMI_saveEmail
             // 
             this.TSMI_saveEmail.Name = "TSMI_saveEmail";
-            this.TSMI_saveEmail.Size = new System.Drawing.Size(180, 22);
+            this.TSMI_saveEmail.Size = new System.Drawing.Size(132, 22);
             this.TSMI_saveEmail.Text = "Сохранить";
             this.TSMI_saveEmail.Click += new System.EventHandler(this.TSMI_saveEmail_Click);
             // 
             // TSMI_getEmails
             // 
             this.TSMI_getEmails.Name = "TSMI_getEmails";
-            this.TSMI_getEmails.Size = new System.Drawing.Size(180, 22);
+            this.TSMI_getEmails.Size = new System.Drawing.Size(132, 22);
             this.TSMI_getEmails.Text = "Получить";
             this.TSMI_getEmails.Click += new System.EventHandler(this.TSMI_getEmails_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Тема";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Дата";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Отправитель";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Содержание";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column3.HeaderText = "Кому";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column6.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column6.HeaderText = "Копия";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column7.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column7.HeaderText = "Скрытая копия";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column8.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Column8.HeaderText = "Теги";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
             // 
             // Form1
             // 
@@ -106,6 +185,14 @@
         private System.Windows.Forms.ToolStripMenuItem письмоToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem TSMI_saveEmail;
         private System.Windows.Forms.ToolStripMenuItem TSMI_getEmails;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
     }
 }
 
--- DV_client/Form1.cs
+++ DV_client/Form1.cs
@@ -31,13 +31,39 @@ namespace DV_client
 
         private void TSMI_getEmails_Click(object sender, EventArgs e)
         {
-            List<Email> emails = (List<Email>)UserControlManager.ActionHandler(new UserControlSettings() {
+            Email[] emails = (Email[])UserControlManager.ActionHandler(new UserControlSettings() {
                 condition = UserControlManager.UserConditions.getEmails
             });
 
-            foreach(Email email in emails)
+            List<User> users = new List<User>((User[])UserControlManager.ActionHandler(new UserControlSettings()
             {
-                DGV_emails.Rows.Add();
+                condition = UserControlManager.UserConditions.getUsers
+            }));
+
+            foreach (Email email in emails)
+            {
+                string to = null;
+                string copy = null;
+                string hidden_copy = null;
+                string tags = null;
+
+                List<User> tmp_users = new List<User>(users.Where(user => email.to.Contains(user.id)));
+                foreach (var user in tmp_users)
+                    to += user.lastname + " " + user.name + " " + user.patronymic + " " + Environment.NewLine;
+
+                //Console.WriteLine("ID = " + email.id);
+                //foreach (int id in email.copy)
+                    //Console.WriteLine("     " + id);
+
+                List<User> tmp_copy = new List<User>(users.Where(user => email.copy.Contains(user.id)));
+                foreach(var user in tmp_copy)
+                    copy += user.lastname + " " + user.name + " " + user.patronymic + " " + Environment.NewLine;
+
+                List<User> tmp_hidden_copy = new List<User>(users.Where(user => email.hidden_copy.Contains(user.id)));
+                foreach (var user in tmp_hidden_copy)
+                    hidden_copy += user.lastname + " " + user.name + " " + user.patronymic + " " + Environment.NewLine;
+
+                DGV_emails.Rows.Add(email.header, email.date, email.from, email.content, to, copy, hidden_copy, tags);
             }
         }
     }
